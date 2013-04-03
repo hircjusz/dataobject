b6364fa36cd5973c6f560dd9009efe27433f7af4
darek.uchman@gmail.com <darek.uchman@gmail.com@96b77cd6-8691-094c-683e-074a416deb8c>
2013-04-03 22:10:57 +0000

    working configuration

---
+++ ConsoleDataObjects/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataObjects.NET;
+
+
+namespace ConsoleDataObjects.Entities
+{
+    public abstract class Person : DataObject
+    {
+        public abstract string Name { get; set; }
+        public abstract string SecondName { get; set; }
+        public abstract string Surname { get; set; }
+        public abstract int Age { get; set; }
+        public abstract string Info { get; set; }
+    }
+}
--- ConsoleDataObjects/Program.cs
+++ ConsoleDataObjects/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using DataObjects.NET;
 
 namespace ConsoleDataObjects
 {
@@ -9,6 +11,33 @@ namespace ConsoleDataObjects
     {
         static void Main(string[] args)
         {
+            BuildDomain();
+        }
+
+        private static Domain BuildDomain()
+        {
+            string connectionUrl = "mssql://darek:gespardmonge@localhost/DataObjects";
+            string productKeyFile = @"D:\SOURCES\dataobjects\ConsoleDataObjects\productKey.txt";
+            string productKey = "";
+            //if (File.Exists(productKeyFile))
+            //    using (StreamReader sr = new StreamReader(productKeyFile))
+            //    {
+            //        productKey = sr.ReadToEnd().Trim();
+            //    }
+
+            Domain domain = DataObjects.NET.Configuration.DefaultDomain;//new Domain(connectionUrl, productKeyFile);
+            // Domain setup process starts here
+            //domain.RegisterCulture(
+            //new Culture("En", "U.S. English", new CultureInfo("en-us", false)));
+            //// It's always necessary to specify default culture
+            //domain.Cultures["En"].Default = true;
+            // Here we register all types from "SampleA.Model" namespace
+            domain.RegisterTypes("ConsoleDataObjects.Entities");
+            // Now we can build the domain. An exclusive access
+            // to domain database should be provided for this period.
+            domain.Build();
+            // Domain setup process is finished here
+            return domain;
         }
     }
 }
